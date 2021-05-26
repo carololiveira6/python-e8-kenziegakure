e835292081c284abe5dd4f8a25f459bff4d088d1
Carolina Oliveira <carolinasao6@gmail.com>
2021-05-26 12:54:14 -0300

    Criadas classes jutsu e ninja

--- src/models/jutsu_model.py
+++ src/models/jutsu_model.py
@@ -4,25 +4,6 @@ class Jutsu():
     def __init__(self, jutsu_name, jutsu_type, jutsu_level, jutsu_damage, chakra_spend):
         self.jutsu_name = jutsu_name
         self.jutsu_type = jutsu_type
-        self.jutsu_level = jutsu_level
+        self.jutsu_level = jutsu_level.upper() if jutsu_level.upper() in self.jutsu_ranks else 'Unranked'
         self.jutsu_damage = jutsu_damage
-        self.chakra_spend = chakra_spend
-
-    def define_level(self, jutsu_ranks):
-        self.jutsu_level = self.jutsu_level.upper()
-
-        for level in jutsu_ranks:
-            if level == self.jutsu_level:
-                self.jutsu_level.upper()
-            else:
-                self.jutsu_level = 'Unranked'       
-
-    def define_chakra_spend(self):
-
-        if self.chakra_spend <= 0:
-            self.chakra_spend = 100
-
-if __name__ == '__main__':
-
-    rasengan = Jutsu('Rasengan', 'Vento', 'a', 20, -15)
-    print(rasengan.__dict__)
+        self.chakra_spend = 100 if chakra_spend <= 0 else chakra_spend
