1aea46b69d2cab2d83ce79b0d8977f7a722d31bc
Carolina Oliveira <carolinasao6@gmail.com>
2021-05-26 13:16:47 -0300

    Concluida Etapa 1

--- src/models/ninja_model.py
+++ src/models/ninja_model.py
@@ -1,3 +1,5 @@
+from jutsu_model import Jutsu
+
 class Ninja():
 
     def __init__(self, name, clan, village, ninja_level = 'Unranked', jutsu_list = [], health_pool = 100, chakra_pool = 100, concious = True):
@@ -11,9 +13,17 @@ class Ninja():
         self.chakra_pool = chakra_pool
         self.concious = concious
 
+    def learn_jutsu(self, jutsu):
+        self.jutsu_list.append(jutsu)
+
+        return 'O ninja Naruto Uzumaki acabou de aprender um novo jutsu: Rasengan'
+
 if __name__ == '__main__':
 
-    naruto = Ninja('Naruto', 'Uzumaki', 'Konoha')
-    narutinho = Ninja('Narutinho', 'Uzumaki', 'Konoha', '5kyiu', ['fire', 'wind'], 30, 40, False)
-    print(naruto.__dict__)
-    print(narutinho.__dict__)
+    # rasengan = Jutsu('Rasengan', 'Vento', 'a', 20, -15)
+    # naruto = Ninja('Naruto', 'Uzumaki', 'Konoha')
+    # narutinho = Ninja('Narutinho', 'Uzumaki', 'Konoha', '5kyiu', ['fire', 'wind'], 30, 40, False)
+    # print(naruto.__dict__)
+    # # print(narutinho.__dict__)
+    # res = naruto.learn_jutsu(rasengan)
+    # print(res)
